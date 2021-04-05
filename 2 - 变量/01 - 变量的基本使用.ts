370f24ef15c8e3306b1199911d4731af0c28e764
GuYun-D <1946306647@qq.com>
2021-04-05 15:51:59 +0800

    ts断点调试

---
+++ 2 - 变量/01 - 变量的基本使用.ts
@@ -0,0 +1,12 @@
+// 声明变量并指定类型
+let age: number
+// 赋值
+age = 18
+
+console.log(age);
+
+// 直接
+let weight: number = 120
+console.log(weight);
+
+// age = "hello"
---
+++ 2 - 变量/02 - 数字类型.ts
@@ -0,0 +1,37 @@
+/**
+ * number
+ *          整数，小数，正数，负数
+ */
+let size: number = 18
+
+let count: number = 99.99
+
+
+console.log(size);
+console.log(count);
+
+/**
+ * 字符串
+ */
+
+console.log("hello");
+console.log('hello');
+console.log(' ');
+
+let str: string = "中国"
+console.log(str);
+
+
+
+/**
+ * 布尔类型
+ */
+let bool: boolean = true
+console.log(bool);
+
+/**
+ * undefined/null
+ */
+let n: undefined = undefined
+
+let m: null = null

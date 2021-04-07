dae61070b0076fca17278cd28a39acc765a025cc
GuYun-D <1946306647@qq.com>
2021-04-07 17:01:58 +0800

    其他数据类型

---
+++ 3 - 数组/04 - 元组类型.ts
@@ -0,0 +1,8 @@
+/**
+ * 元组：tuple
+ * 相当于数组的一种，可以定义多个变量类型
+ */
+
+let tup: [string, number, boolean] = ["元组类型", 12, true]
+
+console.log(tup);
---
+++ 8 - undefined/index.ts
@@ -0,0 +1,10 @@
+let num: undefined;
+console.log(num);
+
+// 报错
+// num = 12
+
+// 定义多个类型
+let num1 : undefined | number ;
+num1 = 12
+console.log(num1);
